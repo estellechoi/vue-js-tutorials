2724735df07ba143d9b3e0523ca47a24a1bed04a
Estelle Choi <estele.choi@gmail.com>
2020-02-28 17:48:22 +0900

    App.vue

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <!-- use component with tag-->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+// import component
 import HelloWorld from './components/HelloWorld.vue'
 
+// Vue instance properties and componet properties can be declared here.
 export default {
   name: 'App',
   components: {
+    // add imported component
+    // 'hello-world': HelloWorld
     HelloWorld
   }
 }
