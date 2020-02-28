431b2ffcb6285546c486057d598f9c4a0d539a79
Estelle Choi <estele.choi@gmail.com>
2020-02-28 17:38:40 +0900

    Sigle File Component

---
+++ vue-cli/src/A.vue
@@ -0,0 +1,21 @@
+<template>
+    <!-- Sigle File Componet -->
+    <!-- vue : .vue file 자동완성 단축키 -->
+    <!-- HTML -->
+    <div>Hello Vue</div>
+</template>
+
+<script>
+export default {
+    // JavaScript
+    methods: {
+        getList: function() {
+
+        }
+    }
+}
+</script>
+
+<style>
+    /* CSS */
+</style>
