4a2e12517c771fe6bdf84e67be8463976ae09db9
Estelle Choi <estele.choi@gmail.com>
2020-03-02 02:17:56 +0900

    form submit using axios

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -16,6 +16,9 @@
 </template>
 
 <script>
+// * axios : http 통신 라이브러리 (npm i axios)
+import axios from 'axios';
+
 export default {
   // Component 간 데이터 공유를 방지하기 위해 data는 객체가 아닌 함수로 작성한다. (값을 참조할 수 없도록)
   data: function() {
@@ -26,8 +29,26 @@ export default {
   },
   methods: {
     submitForm: function() {
-      // evt.preventDefault(); // prevent form submit and reset
-      console.log(this.username,this.password);
+      // prevent form submit and reset
+      // evt.preventDefault(); -> event modifier 사용
+      
+      // REST API 규칙에 따라 아래 URL로 데이터를 보내고 받을 수 있다.
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        username: this.username,
+        password: this.password
+      }
+      
+      axios.post(url, data)
+      // Promise 구문 : .then().catch()
+      // when success
+      .then(function(res) {
+        console.log(res); 
+      })
+      // when error
+      .catch(function(error) {
+        console.log(error);
+      });
     }
   }
 }
