3fee0b9bd8789c18d3fcfdfcaa3f780150b37e86
Estelle Choi <estele.choi@gmail.com>
2020-03-01 18:51:14 +0900

    props array data from parent component

---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <!-- Sigle File Componet -->
+  <header>
+      <h1>{{ propsdata }}</h1>
+  </header>
+</template>
+
+<script>
+// component 태그는 2단어 이상으로 구성한다. 컴포넌트를 참조할 때 html 태그와 중복되는 일을 방지하기 위해서. 
+export default {
+    props: ['propsdata']
+}
+</script>
+
+<style>
+
+</style>
