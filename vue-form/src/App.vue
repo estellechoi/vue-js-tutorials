8600e06c5669eb688c6d549b18d9a76b80db5eb2
Estelle Choi <estele.choi@gmail.com>
2020-03-02 02:01:36 +0900

    form input v-model

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <!-- * .prevent : event modifier -->
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <!-- input is auto focused when its label is clicked -->
+      <label for="username">Id : </label>
+      <input type="text" id="username" v-model="username">
+      <!-- v-model : two-way data binding -->
+    </div>
+    <div>
+      <label for="password">Password : </label>
+      <input type="password" id="password" v-model="password">
+    </div>
+    <button type="submit">Login</button>
+  </form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  // Component 간 데이터 공유를 방지하기 위해 data는 객체가 아닌 함수로 작성한다. (값을 참조할 수 없도록)
+  data: function() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitForm: function() {
+      // evt.preventDefault(); // prevent form submit and reset
+      console.log(this.username,this.password);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
