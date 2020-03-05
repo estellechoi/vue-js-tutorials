56361b2bb713ac53344a45745cc203bb0611736f
Estelle Choi <estele.choi@gmail.com>
2020-03-06 01:30:40 +0900

    v-on:keyup.enter / localStorage / JSON.stringify(object) / Modal used

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -6,15 +6,32 @@
       <!-- awesome icon font -->
       <i class="fas fa-plus addBtn"></i>
     </span>
+
+    <!-- Modal component -->
+    <modal v-if="showModal" @close="showModal = false">
+      <!-- replace <slot name="header">default header</slot> of Modal component. -->
+      <h3 slot="header" class="closeModalBtn">
+        경고 !
+        <i class="fas fa-times" @click="showModal = false"></i>
+      </h3>
+      <div slot="body">할 일을 입력하세요.</div>
+    </modal>
   </div>
 </template>
 
 <script>
+import Modal from "./common/Modal.vue";
+
 export default {
   data: function() {
     return {
-      newTodoItem: ""
+      newTodoItem: "",
+      showModal: false
     };
+  },
+  // modal component added
+  components: {
+    modal: Modal
   },
   methods: {
     addTodo: function() {
@@ -22,6 +39,8 @@ export default {
         // $emit('event', parameter, parameter, ..);
         this.$emit("addNewTodo", this.newTodoItem);
         this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
       }
     },
     clearInput: function() {
@@ -59,5 +78,9 @@ export default {
 .addBtn {
   color: white;
   vertical-align: middle;
+}
+
+.closeModalBtn {
+  color: #42b983;
 }
 </style>
