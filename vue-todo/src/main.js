6a157cdf992183a022d1028e5f6fab3846891db7
Estelle Choi <estele.choi@gmail.com>
2020-03-09 00:11:28 +0900

    Vue instance declared, Vuex added (store)

--- vue-todo/src/main.js
+++ vue-todo/src/main.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
+import { store } from "./store/store.js";
 
 new Vue({
   el: "#app",
+  // add store (Vuex)
+  store,
   render: h => h(App)
 });
