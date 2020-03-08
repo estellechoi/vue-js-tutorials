1487a9db74111a0487a201656f3fa10477fc16fd
Estelle Choi <estele.choi@gmail.com>
2020-03-09 00:07:29 +0900

    Vue.use(Vuex);

---
+++ vue-todo/src/store/store.js
@@ -0,0 +1,9 @@
+// import libraries from node_modules.
+import Vue from "vue";
+import Vuex from "vuex";
+
+// When Vuex used with a module system, you must explicitly install Vuex via Vue.use();
+// Vue.use(); 뷰 어디에서나 해당 플러그인을 사용할 수 있도록 전역 사용하기로 지정한다.
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({});
