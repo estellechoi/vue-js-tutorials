2cb9436bcb65df12fb894bad7afa99e0bd552851
Estelle Choi <estele.choi@gmail.com>
2020-03-16 18:16:37 +0900

    demo files to test mapGetter Helper

---
+++ vue-todo/src/components/demo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="root">
+    <p>{{ originalPrice }}</p>
+    <p>{{ doublePrice }}</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapGetters(["originalPrice", "doublePrice"])
+  }
+};
+</script>
+
+<style>
+</style>
