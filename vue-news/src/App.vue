11ed299a300985c04bf3a0533d9234b531fee9e5
Estelle Choi <estele.choi@gmail.com>
2020-04-10 15:45:01 +0900

    vue-router routes components according to url

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <div id="app"></div>
+	<div id="app">
+		<!-- <router-view> 요소는 url 에 해당하는 컴포넌트 태그로 대체된다. -->
+		<router-view></router-view>
+	</div>
 </template>
 
 <script>
@@ -8,11 +11,11 @@ export default {};
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
--- vue-news/src/router/index.js
+++ vue-news/src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import { News } from "../views/News.vue";
-import { Ask } from "../views/Ask.vue";
-import { Jobs } from "../views/Jobs.vue";
+import News from "../views/News.vue";
+import Ask from "../views/Ask.vue";
+import Jobs from "../views/Jobs.vue";
 
 Vue.use(VueRouter);
 
