940a2db467ba18a6413a709794d2da131708a774
Estelle Choi <estele.choi@gmail.com>
2020-04-13 14:06:09 +0900

    dynamic matching of router

--- vue-news/src/api/index.js
+++ vue-news/src/api/index.js
@@ -18,5 +18,9 @@ function fetchUserInfo(userId) {
 	// https://api.hnpwa.com/v0/user/davideast.json
 	return axios.get(`${config.baseUrl}users/${userId}.json`);
 }
+function fetchAsk(id) {
+	// https://api.hnpwa.com/v0/item/13831370.json
+	return axios.get(`${config.baseUrl}item/${id}.json`);
+}
 
-export { fetchNewsList, fetchAskList, fetchJobsList, fetchUserInfo };
+export { fetchNewsList, fetchAskList, fetchJobsList, fetchUserInfo, fetchAsk };
