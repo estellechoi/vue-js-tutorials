4306e562b1039c8df9b51c2c0747a7cceb9b1fc3
Estelle Choi <estele.choi@gmail.com>
2020-04-13 19:03:38 +0900

    Router transition added

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,24 +1,35 @@
 <template>
-	<div id="app">
-		<!-- cmd + click -->
-		<menu-bar></menu-bar>
-		<!-- <router-view> 요소는 url 에 해당하는 컴포넌트 태그로 대체된다. -->
-		<router-view></router-view>
-	</div>
+  <div id="app">
+    <!-- cmd + click -->
+    <menu-bar></menu-bar>
+    <transition name="page">
+      <!-- <router-view> 요소는 url 에 해당하는 컴포넌트 태그로 대체된다. -->
+      <router-view></router-view>
+    </transition>
+  </div>
 </template>
 
 <script>
 import MenuBar from "./components/MenuBar.vue";
 export default {
-	components: {
-		MenuBar
-	}
+  components: {
+    MenuBar
+  }
 };
 </script>
 
 <style>
 body {
-	margin: 0;
-	padding: 0;
+  margin: 0;
+  padding: 0;
+}
+
+/* Router Transition */
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
+}
+.page-enter, .page-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
