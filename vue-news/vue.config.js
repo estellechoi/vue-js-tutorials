5b2ea0198680c4faff1a4121442fa8c4c4221173
Estelle Choi <estele.choi@gmail.com>
2020-04-26 22:15:23 +0900

    http Proxy middleware - failed.

--- vue-news/vue.config.js
+++ vue-news/vue.config.js
@@ -5,10 +5,13 @@ module.exports = {
 	// let the dev server proxy all API requests to the actual backend
 	devServer: {
 		proxy: {
-			// proxy all requests starting with /v0 to https://api.hnpwa.com
+			// proxy all requests starting with /api to https://api.hnpwa.com
 			"/v0": {
-				target: "https://api.hnpwa.com",
+				target: "http://api.hnpwa.com",
 				changeOrigin: true,
+				pathRewrite: {
+					"^/v0": "",
+				},
 				// filter: function(pathname, req) {
 				// 	return pathname.match("^/api") && req.method === "GET";
 				// },
