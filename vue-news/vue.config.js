6b7fa39d40c53ba24ed36b876755a199977e9589
Estelle Choi <estele.choi@gmail.com>
2020-04-26 21:54:51 +0900

    devServer proxy added to avoid problems caused by CORS

--- vue-news/vue.config.js
+++ vue-news/vue.config.js
@@ -1,5 +1,18 @@
 module.exports = {
 	// If you want to disable eslint-loader during production build, you can use the following config
 	// lintOnSave: process.env.NODE_ENV !== "production"
-	lintOnSave: true
+	lintOnSave: true,
+	// let the dev server proxy all API requests to the actual backend
+	devServer: {
+		proxy: {
+			// proxy all requests starting with /v0 to https://api.hnpwa.com
+			"/v0": {
+				target: "https://api.hnpwa.com",
+				changeOrigin: true,
+				// filter: function(pathname, req) {
+				// 	return pathname.match("^/api") && req.method === "GET";
+				// },
+			},
+		},
+	},
 };
