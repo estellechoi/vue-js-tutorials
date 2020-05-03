d962311f2ad40514b7ad4375571a537b92d9e88b
Estelle Choi <estele.choi@gmail.com>
2020-05-03 19:56:14 +0900

    Receiving events through Bus

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -6,18 +6,41 @@
       <!-- <router-view> 요소는 url 에 해당하는 컴포넌트 태그로 대체된다. -->
       <router-view></router-view>
     </transition>
-    <spinner :loading="true"></spinner>
+    <spinner :loading="isLoading"></spinner>
   </div>
 </template>
 
 <script>
+import bus from "./utils/bus.js";
 import Spinner from "./components/Spinner.vue";
-
 import MenuBar from "./components/MenuBar.vue";
+
 export default {
   components: {
     Spinner,
     MenuBar
+  },
+  data() {
+    return {
+      isLoading: false
+    };
+  },
+  methods: {
+    startSpinner() {
+      this.isLoading = true;
+    },
+    endSpinner() {
+      this.isLoading = false;
+    }
+  },
+  created() {
+    bus.$on("start:spinner", this.startSpinner);
+    bus.$on("end:spinner", this.endSpinner);
+  },
+  // to prevent event objects from stacking.
+  beforeDistroy() {
+    bus.$off("start:spinner", this.startSpinner);
+    bus.$off("end:spinner", this.endSpinner);
   }
 };
 </script>
