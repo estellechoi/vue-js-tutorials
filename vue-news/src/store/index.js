45e9b0c53bdca6e3119462bb98d7af257b157dfc
Estelle Choi <estele.choi@gmail.com>
2020-05-07 22:11:20 +0900

    vuex state, getters

--- vue-news/src/store/index.js
+++ vue-news/src/store/index.js
@@ -9,9 +9,6 @@ Vue.use(Vuex);
 // Vuex : 상태 관리 도구 (상태: 컴포넌트간 공유되는 데이터 속성)
 export const store = new Vuex.Store({
 	state: {
-		// news: [],
-		// jobs: [],
-		// asks: [],
 		userInfo: {},
 		ask: {},
 		list: [],
