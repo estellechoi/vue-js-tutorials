845d7a313bb5fdf0edaaf71dba22b01260e63bbe
Estelle Choi <estele.choi@gmail.com>
2020-05-07 22:12:24 +0900

    vuex actions, mutations

--- vue-news/src/store/actions.js
+++ vue-news/src/store/actions.js
@@ -17,30 +17,6 @@ export default {
 			})
 			.catch((err) => console.log(err));
 	},
-	// FETCH_NEWS(context) {
-	// 	fetchNewsList()
-	// 		.then((res) => {
-	// 			context.commit("SET_NEWS", res.data); // commit to mutations
-	// 			return res;
-	// 		})
-	// 		.catch((err) => console.log(err));
-	// },
-	// FETCH_JOBS({ commit }) {
-	// 	fetchJobsList()
-	// 		.then(({ data }) => {
-	// 			commit("SET_JOBS", data);
-	// 		})
-	// 		.catch((err) => console.log(err));
-	// },
-	// FETCH_ASKS(context) {
-	// 	fetchAskList()
-	// 		.then((res) => {
-	// 			context.commit("SET_ASKS", res.data);
-	// 			return res;
-	// 		})
-	// 		.catch((err) => console.log(err));
-	// },
-
 	// 파라미터가 객체일 때 { } 를 사용하여 객체 내의 속성에 바로 접근할 수 있다.
 	FETCH_USERINFO({ commit }, userId) {
 		fetchUserInfo(userId)
--- vue-news/src/store/mutations.js
+++ vue-news/src/store/mutations.js
@@ -4,15 +4,6 @@ export default {
 	SET_LIST(state, data) {
 		state.list = data;
 	},
-	// SET_NEWS(state, data) {
-	// 	state.news = data;
-	// },
-	// SET_JOBS(state, data) {
-	// 	state.jobs = data;
-	// },
-	// SET_ASKS(state, data) {
-	// 	state.asks = data;
-	// },
 	SET_USER(state, data) {
 		state.userInfo = data;
 	},
